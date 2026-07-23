3584b964b08907acd58cf7df99fc4182a2f912a2
Anastasiia Kurdiukova <anastasiia.kurdiukova@cleveroad.com>
2026-07-23 21:48:47 +0300

    initial comit

---
+++ index.js
@@ -0,0 +1,22 @@
+class ECTS {
+  constructor(score) {
+    this.score = score;
+  }
+
+  ectsFromScore() {
+    if (this.score >= 90 && this.score <= 100) {
+      return "A";
+    } else if (this.score >= 82 && this.score <= 89) {
+      return "B";
+    } else if (this.score >= 74 && this.score <= 81) {
+      return "C";
+    } else if (this.score >= 65 && this.score <= 73) {
+      return "D";
+    } else if (this.score >= 60 && this.score <= 64) {
+      return "E";
+    }
+    return F;
+  }
+}
+
+export { ECTS };
